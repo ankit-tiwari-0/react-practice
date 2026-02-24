1e912e7ef0654801edc3f7c4361b78ecfe95cc63
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-02-24 18:37:54 +0530

    Multiple-cards

--- 02-react/src/App.jsx
+++ 02-react/src/App.jsx
@@ -1,11 +1,117 @@
-import React from 'react'
-import Cards from './components/cards'
-
+import Multiple from './components/multiple-card'
 const App = () => {
+const jobOpenings = [
+  {
+    brandLogo: "https://static.vecteezy.com/system/resources/previews/013/734/184/original/3d-google-logo-google-is-usa-multinational-corporation-free-vector.jpg",
+    companyName: "Google",
+    datePosted: "5 days ago",
+    post: "Frontend Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$45/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://tse1.mm.bing.net/th/id/OIP.-HIrpRMx0kqqSEVJxOFMsgHaEB?rs=1&pid=ImgDetMain&o=7&rm=3",
+    companyName: "Apple",
+    datePosted: "2 weeks ago",
+    post: "iOS Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$70/hr",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://tse2.mm.bing.net/th/id/OIP.AuggztWv8Hwzt2zjbJcztAHaEK?rs=1&pid=ImgDetMain&o=7&rm=3",
+    companyName: "Amazon",
+    datePosted: "10 days ago",
+    post: "Backend Engineer",
+    tag1: "Part Time",
+    tag2: "Junior Level",
+    pay: "$50/hr",
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://i.pinimg.com/originals/21/ff/0d/21ff0dec3b8043807d8a335aafd24f8d.png",
+    companyName: "Meta",
+    datePosted: "1 week ago",
+    post: "React Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$65/hr",
+    location: "Gurgaon, India"
+  },
+  {
+    brandLogo: "https://tse1.mm.bing.net/th/id/OIP.Of2MrfKXGN1chuR1adiytQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3",
+    companyName: "Netflix",
+    datePosted: "3 days ago",
+    post: "UI Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$80/hr",
+    location: "Remote"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/1024px-Microsoft_logo.svg.png?20210729021049",
+    companyName: "Microsoft",
+    datePosted: "4 weeks ago",
+    post: "Full Stack Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$55/hr",
+    location: "Noida, India"
+  },
+  {
+    brandLogo: "https://tse1.mm.bing.net/th/id/OIP.zf8g1tolI3HRPumaeETgcAHaEK?rs=1&pid=ImgDetMain&o=7&rm=3",
+    companyName: "Tesla",
+    datePosted: "6 days ago",
+    post: "Software Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$75/hr",
+    location: "Pune, India"
+  },
+  {
+    brandLogo: "https://tse2.mm.bing.net/th/id/OIP.XDxTk0xxRfEKu_2iIkMx0QHaEK?rs=1&pid=ImgDetMain&o=7&rm=3",
+    companyName: "Adobe",
+    datePosted: "9 days ago",
+    post: "JavaScript Developer",
+    tag1: "Part Time",
+    tag2: "Junior Level",
+    pay: "$48/hr",
+    location: "Delhi, India"
+  },
+  {
+    brandLogo: "https://freelogopng.com/images/all_img/1659761297uber-icon.png",
+    companyName: "Uber",
+    datePosted: "2 days ago",
+    post: "Mobile App Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$68/hr",
+    location: "Chennai, India"
+  },
+  {
+    brandLogo: "https://tse3.mm.bing.net/th/id/OIP.jdQ0-zCqys8HUsVr1-EE6AHaEK?rs=1&pid=ImgDetMain&o=7&rm=3",
+    companyName: "Oracle",
+    datePosted: "3 weeks ago",
+    post: "Cloud Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$72/hr",
+    location: "Kolkata, India"
+  }
+];
   return (
     <div className='parent'>
-
-     <Cards />
+     {
+      jobOpenings.map(function(elem, idx){
+        return <div key={idx}> <Multiple com = {elem.companyName} location = {elem.location}
+      pay = {elem.pay} tag1 ={elem.tag1} tag2 ={elem.tag2} post ={elem.post} 
+       datePosted = {elem. datePosted} brandLogo = {elem.brandLogo}/>
+      </div>
+      })
+     }    
     </div>
   )
 }
---
+++ 02-react/src/components/multiple-card.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Bookmark} from 'lucide-react'
+const Multiple = (props) => {
+  return (
+        <div className='cards'>
+      <div className='top'>
+       <img src={props.brandLogo} alt="" />
+       <button>save <Bookmark  size={15} /></button>
+       <div>
+        
+       </div>
+      </div>
+      <div className='center'>
+       <h3> {props.com} <span>{props. datePosted}</span></h3>
+       <h2>{props.post}</h2> 
+       <div className='tag'>
+        <h4>{props.tag1}</h4>
+        <h4>{props.tag2}</h4>
+        </div>  
+      </div>
+      <div className="bottom">
+          <div>
+            <h3>{props.pay}</h3>
+            <p>{props.location}</p>
+          </div>
+          <button>Apply now</button>
+      </div>
+     </div>
+  )
+}
+
+export default Multiple
