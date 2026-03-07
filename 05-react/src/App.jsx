440e0d97af800e1a055a57950dd77a851e4b7947
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-07 21:44:44 +0530

    practice arry and obj

---
+++ 05-react/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  //**Object */ const [num, setnum] = useState({user:'Ankit', age:20});
+ 
+  //  const clickch = () => {
+  //   const  newnum = {...num};
+  //   newnum.user = "Noorin"
+  //   newnum.age = 20
+  //   setnum(newnum)
+  //  }  
+
+  const [arry, setarry] = useState([10,20,40])
+
+  const btnclick = () =>{
+    const newarray = [...arry]
+    newarray.push(100)
+
+    setarry(newarray)
+  }
+
+  return (
+    <div>
+      <h1>{arry}</h1>
+      <button onClick={btnclick}>click</button>
+      {/* <button onClick={clickch}>Click</button> object*/}
+    </div>
+  )
+}
+
+export default App
