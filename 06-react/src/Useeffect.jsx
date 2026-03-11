9f412d25216c191c92bb09670195c23da1a41e88
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-11 17:47:13 +0530

    useeffect

---
+++ 06-react/src/Useeffect.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+    const [num, setnum] = useState(0)
+    const [nu2, setnu2] = useState(500)
+
+    useEffect (function () {
+        console.log('useeffect is running ');
+        
+    }, [num])
+  return (
+    <div>
+     <h1>{num}</h1>
+     <h1>{nu2}</h1>
+     <button onMouseEnter={()=>{
+        setnum(num+1)
+     }} 
+     onMouseLeave={()=>{
+        setnu2(nu2+100)
+     }}>Click</button>
+
+    </div>
+  )
+}
+
+export default App
