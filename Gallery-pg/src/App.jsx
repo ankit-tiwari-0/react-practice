0604950400ab45374fb9141e5da686db1dfa2804
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-12 19:48:44 +0530

    git improv api performance

--- Gallery-pg/src/App.jsx
+++ Gallery-pg/src/App.jsx
@@ -13,15 +13,15 @@ const response = await   axios.get(`https://picsum.photos/v2/list?page=${index}&
 
 useEffect(function(){
   getdata()
-})
+},[index])
 
-let printdata = <h3 className='text-gray-400 text-2xl'>Loading....</h3>
+let printdata = <h3 className='text-gray-400 font-bold text-3xl absolute top-1/2 -translate-1/2 -translate-x-1/2'>Loading....</h3>
 if(data.length>0){
   printdata = data.map(function(ele,idx) {
     return <div  key={idx}>
-      <a href={ele.url} target='-blank'>
+      <a href={ele.url} target='_blank'>
         <div className='lg:h-40 lg:w-44 h-30 w-30 overflow-hidden rounded-2xl'>
-           <img className='h-full w-full object-cover'  src={ele.download_url} alt="" />    
+           <img className='h-full w-full object-cover'  src={`https://picsum.photos/id/${ele.id}/300/200`} alt={ele.author} />    
         </div>
         <h2 className='font-bold lg:text-lg text-sm'>{ele.author}</h2>
       </a>
@@ -41,12 +41,14 @@ if(data.length>0){
           <button onClick={()=>{
            if (index>1){
             setindex(index-1)
+            setdata([])
            }
-          }} className='bg-amber-300 text-sm cursor-pointer active:scale-95 px-4 py-2 font-semibold rounded-xl'>Prev</button>
-          <h1>{index}</h1>
+          }} className='bg-amber-300 hover:bg-red-400 text-sm cursor-pointer active:scale-150  px-4 py-2 font-semibold rounded-xl '>Prev</button>
+          <h1 className='font-bold text-xl text-amber-100'>{index}</h1>
           <button onClick={()=>{
            setindex(index+1)
-          }} className='bg-amber-300 text-sm cursor-pointer active:scale-95 px-4 py-2 font-semibold rounded-xl'>Next</button>
+           setdata([])
+          }} className='bg-amber-300 hover:bg-pink-500 text-sm cursor-pointer active:scale-150  px-4 py-2 font-semibold rounded-xl '>Next</button>
          </div>
     </div>
   )
