e7e2510cb7d33caec860577a0fab6b3ab3e1f699
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-13 13:00:51 +0530

    git add NotFound

--- 07 route/src/App.jsx
+++ 07 route/src/App.jsx
@@ -5,6 +5,7 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Nav from './compo/Nav'
 import Footer from './compo/Footer'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
   )
