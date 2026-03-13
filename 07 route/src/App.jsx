3f3f7c66ff3d2ad2e83f461ad8a1e3757c25b380
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-03-13 11:59:11 +0530

    Nav

--- 07 route/src/App.jsx
+++ 07 route/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/about' element={<Contact />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ 07 route/src/compo/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='nav'>
+         <h2>Ankit</h2>
+    </div>
+  )
+}
+
+export default Nav
